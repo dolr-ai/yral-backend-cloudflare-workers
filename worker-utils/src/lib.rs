f7b86feb6cae57e1c044bfe2d39725c0cacc902f
Rupansh Sekar <155418402+rupansh-sekar-yral@users.noreply.github.com>
2025-08-04 15:18:27 +0000

    chore(worker-utils): lint check

--- worker-utils/src/lib.rs
+++ worker-utils/src/lib.rs
@@ -59,11 +59,9 @@ macro_rules! parse_principal {
     }};
 }
 
-
 pub fn err_to_resp<E>(status_code: u16, e: E) -> worker::Result<worker::Response>
 where
     E: Serialize,
 {
     Ok(worker::Response::from_json(&e)?.with_status(status_code))
 }
-
